b2453169bc84e4ec06e048e8784f75bbea189fec
Amanda Olczak <aolczak@gmail.com>
2016-08-07 00:23:16 -0400

    injected ui-router dependency and set up routing for basic view partials

--- public/js/modules/cooking_connect.js
+++ public/js/modules/cooking_connect.js
@@ -1,1 +1,20 @@
-var app = angular.module("cookingConnect", ["ngRoute"]);
+var app = angular.module("cookingConnect", ["ui.router"]);
+
+app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider
+    .otherwise("/landing");
+
+  $stateProvider
+    .state("landing", {
+      url: "/landing",
+      templateUrl: "partials/landing.html"
+    })
+    .state("userhome", {
+      url: "/userhome",
+      templateUrl: "partials/userhome.html"
+    })
+    .state("cookbook", {
+      url: "/cookbook",
+      templateUrl: "partials/cookbook.html"
+    });
+}]);
