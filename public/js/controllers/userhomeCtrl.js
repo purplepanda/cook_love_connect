6fbda97a88a8a9c52535cbb4badccdd1e71e1ff8
John Benson <johnbenson@gmail.com>
2016-08-08 19:48:07 -0400

    recipe search function and parameter defined

--- public/js/controllers/userhomeCtrl.js
+++ public/js/controllers/userhomeCtrl.js
@@ -5,5 +5,13 @@ app.controller("userhomeCtrl", function($scope) {
 
   $scope.recipeForm = function() {
     alert("This should pop up the new reciple form");
+  };
+
+// benson attempting to define function that is called on submit button ng-click event
+  $scope.submitSearch = function(searchIt){
+    console.log(searchIt);
+    // actually, probably need another step here to make http call to API before saving object
+    // searchFactory.saveObject(searchIt);
   }
+
 });
--- public/js/services/get_recipe_service.js
+++ public/js/services/get_recipe_service.js
@@ -1,1 +1,24 @@
 //placeholder for future Service code, please rename when appropriate
+
+// var app = angular.module('cookingConnect');
+//
+// app.factory('searchFactory', function(){
+//   var factoryObject = {};
+//   var bensonVar = 'BENSON is TESTING';
+  // GETTER
+  // function saveObject(searchIt){
+    // grab data from searchIt and make available to this factory for use in SETTER
+  //   factoryObject = searchIt;
+  //   console.log('factoryObject: ' + factoryObject);
+  // }
+
+  // SETTER
+  // function returnObject(){
+  //   return factoryObject;
+  // }
+
+//   return {
+//     saveObject: saveObject,
+//     returnObject: returnObject
+//   };
+// });
