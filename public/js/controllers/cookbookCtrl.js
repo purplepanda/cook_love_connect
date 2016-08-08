9f84ecd3deb758f027a73bdc6550b97ebe457003
Amanda Olczak <aolczak@gmail.com>
2016-08-08 15:03:41 -0400

    tried alternate get request, unsuccessful, revert to original

--- public/js/controllers/cookbookCtrl.js
+++ public/js/controllers/cookbookCtrl.js
@@ -3,18 +3,25 @@ var app = angular.module("cookingConnect");
 app.controller("cookbookCtrl", function($scope, $http) {
   $scope.title = "The ... Cookbook";
 
+  // $.ajax({
+  //   url: 'http://food2fork.com/api/search?key=fc28481c5019cd053fe8ad5794e34d40&q=shredded%20chicken',
+  //   dataType: 'jsonp',
+  //   crossDomain: true
+  // }).done(function(response) {
+  //   console.log(response);
+  // }.bind(this));
+
   $http({
     method: "GET",
+    dataType: 'jsonp',
+    crossDomain: true,
     url: 'http://food2fork.com/api/search?key=fc28481c5019cd053fe8ad5794e34d40&q=shredded%20chicken'
   }).then(function successCallback(response) {
-        // $scope.badges = response.data.badges
-        console.log(response);
-      // this callback will be called asynchronously
-      // when the response is available
-    }, function errorCallback(response) {
-      console.log('something went wrong');
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });
+    // $scope.badges = response.data.badges
+    console.log(response);
+  }, function errorCallback(response) {
+    console.log('something went wrong');
+
+  });
 
 });
