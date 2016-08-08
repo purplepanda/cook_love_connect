7931d06cc3844e1e3c5c9a384e6ad36e540dc84a
Amanda Olczak <aolczak@gmail.com>
2016-08-08 17:21:02 -0400

    added dummy recipe form in the hopes that I can attached it to a database

--- public/js/controllers/userhomeCtrl.js
+++ public/js/controllers/userhomeCtrl.js
@@ -3,7 +3,7 @@ var app = angular.module("cookingConnect");
 app.controller("userhomeCtrl", function($scope) {
   $scope.title = "My Kitchen";
 
-  $scope.recipeForm = function() {
-    alert("This should pop up the new reciple form");
-  }
+  $scope.submitRecipe = function() {
+    alert("This should POST the new recipe info to the database!");
+  };
 });
