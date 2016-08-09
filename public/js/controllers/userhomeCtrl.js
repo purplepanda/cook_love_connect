c97250106c07ab1e27a2176339c48873e4b356ed
John Benson <johnbenson@gmail.com>
2016-08-09 10:35:16 -0400

    temp commit so i can look at master w group

--- public/js/controllers/userhomeCtrl.js
+++ public/js/controllers/userhomeCtrl.js
@@ -1,7 +1,21 @@
 var app = angular.module("cookingConnect");
 
-app.controller("userhomeCtrl", ['$scope', '$http', 'searchFactory', function($scope, $http, searchFactory) {
+app.controller("userhomeCtrl", ['$scope', '$http', function($scope, $http) {
   $scope.title = "My Kitchen";
+
+  // josh added this code block Aug 9 AM
+  $http({
+    method: "GET",
+    dataType: 'jsonp',
+    crossDomain: true,
+    url: 'http://recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3'
+  }).then(function successCallback(response) {
+    // $scope.badges = response.data.badges
+    console.log(response);
+  }, function errorCallback(response) {
+    console.log('something went wrong');
+
+  });
 
   $scope.recipeForm = function() {
     alert("This should pop up the new reciple form");
@@ -12,11 +26,12 @@ app.controller("userhomeCtrl", ['$scope', '$http', 'searchFactory', function($sc
   $scope.submitSearch = function(searchIt){
     console.log(searchIt);
     tempSearch = searchIt;
+    console.log('tempSearch:', tempSearch);
+
+
     // actually, probably need another step here to make http call to API before saving object
-    // Simple GET request example:
 
-
-
+    // including this next line throws an error that saveObject is not a function, even if I define it in the factory
     // searchFactory.saveObject(searchIt);
   };
 
--- public/js/modules/cooking_connect.js
+++ public/js/modules/cooking_connect.js
@@ -23,23 +23,5 @@ app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $ur
 }]);
 
 // [benson] currently, this does not work if I cut / paste into get_recipe_service -- throws an injector error
-app.factory('searchFactory',['$http', function($http){
-  var urlBase = 'http://recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3';
-  var searchFactory = {};
-  searchFactory.getRecipe = function(){
-    return $http.get(urlBase);
-  };
 
-
-  console.log("searchFactory ", searchFactory)
-  return searchFactory;
-  // PREVIOUS, BROKEN CODE -- delete once we get the above into separate factory file
-  // $http({
-  //   method: 'GET',
-  //   url: 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3'
-  // }).then(function successCallback(response) {
-  //     console.log('http service res: ', response);
-  //   }, function errorCallback(response) {
-  //
-  //   });
-}]);
+// this helped  http://weblogs.asp.net/dwahlin/using-an-angularjs-factory-to-interact-with-a-restful-service
