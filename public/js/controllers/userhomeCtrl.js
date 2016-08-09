cdb3a8d906a2f500617520ced62c60869112f568
John Benson <johnbenson@gmail.com>
2016-08-08 20:18:24 -0400

    added factory but not working or injecting properly

--- public/js/controllers/userhomeCtrl.js
+++ public/js/controllers/userhomeCtrl.js
@@ -1,17 +1,23 @@
 var app = angular.module("cookingConnect");
 
-app.controller("userhomeCtrl", function($scope) {
+app.controller("userhomeCtrl", function($scope, $http, searchFactory) {
   $scope.title = "My Kitchen";
 
   $scope.recipeForm = function() {
     alert("This should pop up the new reciple form");
   };
 
-// benson attempting to define function that is called on submit button ng-click event
+// [benson] define function that is called on submit button ng-click event
+  var tempSearch;
   $scope.submitSearch = function(searchIt){
     console.log(searchIt);
+    tempSearch = searchIt;
     // actually, probably need another step here to make http call to API before saving object
+    // Simple GET request example:
+
+
+
     // searchFactory.saveObject(searchIt);
-  }
+  };
 
 });
--- public/js/services/get_recipe_service.js
+++ public/js/services/get_recipe_service.js
@@ -1,7 +1,22 @@
 //placeholder for future Service code, please rename when appropriate
 
-// var app = angular.module('cookingConnect');
-//
+var app = angular.module('cookingConnect');
+app.factory('searchFactory',['$http', function($http){
+  $http({
+    method: 'GET',
+    url: 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3'
+  }).then(function successCallback(response) {
+      // currently response is
+      console.log('http service res: ', response);
+    }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
+}]);
+
+
+
+
 // app.factory('searchFactory', function(){
 //   var factoryObject = {};
 //   var bensonVar = 'BENSON is TESTING';
