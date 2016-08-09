42c226731a2604e859fd4f484a2228dfb2ae8d7f
John Benson <johnbenson@gmail.com>
2016-08-09 15:05:28 -0400

    styling signup form

--- public/js/controllers/landingCtrl.js
+++ public/js/controllers/landingCtrl.js
@@ -3,12 +3,14 @@ var app = angular.module("cookingConnect");
 app.controller("landingCtrl", function($scope) {
   $scope.title = "Welcome to Cook Love Connect!";
 
+
   $scope.loginTemp = function() {
     alert('Temporary Function! Clicking this button should pop up a form to login');
   };
 
   $scope.regTemp = function() {
-    alert('Temporary Function! Clicking this button should pop up a form to register');
+    // alert('Temporary Function! Clicking this button should pop up a form to register');
+    $('#signup').show();
   };
 
 });
