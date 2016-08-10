b5f9ea863174d0e462c1880b7defa2312b65b7d5
Josh Teo <joshua.teoleekuok@gmail.com>
2016-08-10 10:39:36 -0400

    modal form

--- public/js/controllers/userhomeCtrl.js
+++ public/js/controllers/userhomeCtrl.js
@@ -3,7 +3,7 @@ var app = angular.module("cookingConnect");
 app.controller("userhomeCtrl", function($scope) {
   $scope.title = "My Kitchen";
 
-  $scope.recipeForm = function() {
-    alert("This should pop up the new reciple form");
-  }
+  // $scope.recipeForm = function() {
+  //   alert("This should pop up the new reciple form");
+  // }
 });
