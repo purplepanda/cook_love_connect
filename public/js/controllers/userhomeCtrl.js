8b7c903602e1d5ce35300cbc0f61add793c7a905
John Benson <johnbenson@gmail.com>
2016-08-10 21:25:39 -0400

    api search returns objects, but doesn't go anywhere yet

--- public/js/controllers/userhomeCtrl.js
+++ public/js/controllers/userhomeCtrl.js
@@ -4,21 +4,21 @@ app.controller("userhomeCtrl", ['$scope', '$http', function($scope, $http) {
   $scope.title = "My Kitchen";
 
   // josh added this code block Aug 9 AM
-  $http({
-    method: "GET",
-    dataType: 'jsonp',
-    crossDomain: true,
-    url: 'http://recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3'
-  }).then(function successCallback(response) {
-    // $scope.badges = response.data.badges
-    console.log(response);
-  }, function errorCallback(response) {
-    console.log('something went wrong');
-
-  });
+  // $http({
+  //   method: "GET",
+  //   dataType: 'jsonp',
+  //   crossDomain: true,
+  //   url: 'http://recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3'
+  // }).then(function successCallback(response) {
+  //   // $scope.badges = response.data.badges
+  //   console.log(response);
+  // }, function errorCallback(response) {
+  //   console.log('something went wrong');
+  //
+  // });
 
   $scope.recipeForm = function() {
-    alert("This should pop up the new reciple form");
+    alert("This should pop up the new recipe form");
   };
 
 // [benson] define function that is called on submit button ng-click event
@@ -26,17 +26,24 @@ app.controller("userhomeCtrl", ['$scope', '$http', function($scope, $http) {
   $scope.submitSearch = function(searchIt){
     // console.log(searchIt);
     tempSearch = searchIt;
-    console.log('tempSearch:', tempSearch);
+    // console.log('tempSearch:', tempSearch);
 
-  // use tempSearch to make API call
+  // make API call by concat urlbase w/ user-supplied search query
     var urlBase = 'http://recipepuppy.com/api/?q=';
     var searchUrl = urlBase + tempSearch;
-    console.log("urlBase is ", urlBase);
-    console.log("searchUrl is ", searchUrl);
+    // console.log("urlBase is ", urlBase);
+    // console.log("searchUrl is ", searchUrl);
+
+    $http.get(searchUrl)
+    .then(function(response){
+      console.log("successful API request!", response);
+      $scope.thirdpartyrecipes = response.results; });
+
     var searchFactory = {};
     searchFactory.getRecipe = function(){
       console.log('tempSearch inside getRecipe function', tempSearch);
-      return $http.get(urlBase);
+      // return $http.get(urlBase);
+      return $http.get(searchUrl);
     };
 
     // including this next line throws an error that saveObject is not a function, even if I define it in the factory
