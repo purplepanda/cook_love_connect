600f86b1cf8c97c65f47710b4e6bd659ede7ff91
John Benson <johnbenson@gmail.com>
2016-08-13 11:42:56 -0400

    working auth system :)

--- public/js/controllers/landingCtrl.js
+++ public/js/controllers/landingCtrl.js
@@ -21,6 +21,54 @@ app.controller("landingCtrl", function($scope, $state) {
 
   $scope.fakeLogin = function() {
     $state.go("userhome");
-  }
+  };
+
+  // firebase code aug 13 //
+
+  // Get elements
+
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogout = document.getElementById('btnLogout');
+
+  // Add login event
+  btnLogin.addEventListener('click', e => {
+    // Get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  });
+
+  btnSignUp.addEventListener('click', e => {
+    // Get email and pass
+    // TODO: check for real email address
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise
+      .catch(e => console.log(e.message));
+  });
+
+  btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+  });
+
+  // Add a realtime listener
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+      console.log(firebaseUser);
+      btnLogout.classList.remove('hide');
+    } else {
+      console.log('not logged in');
+      btnLogout.classList.add('hide');
+    }
+  });
 
 });
